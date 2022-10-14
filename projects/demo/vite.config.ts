ba6928972479a4386fe9478cc457b9555e81395b
ZTStory <m18740395237@163.com>
2022-10-14 11:38:00 +0800

    🐞 fix: ci

--- projects/demo/vite.config.ts
+++ projects/demo/vite.config.ts
@@ -20,7 +20,7 @@ const pxtovw_config = pxtovw({
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV !== "production" ? "/vue-datav/" : "",
+    base: process.env.NODE_ENV === "production" ? "/vue-datav/" : "/",
     plugins: [
         vue(),
         // gzip压缩
